33992d6e2bdaa81aa01feda68af84778935f2399
gassig <giorgiagassi@gmail.com>
2024-06-11 10:28:18 +0200

    fix finale

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.esempio.addo',
-  appName: 'addo_app',
+  appName: 'Addò',
   webDir: 'www'
 };
 
